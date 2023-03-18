2b234842cf94941caae099539e8bdf8df8dc9848
Jinga <info.alpain@gmail.com>
2023-03-19 07:35:02 +0900

    <add> initial commit

---
+++ 02_deckgl_maplibre_interleaved/src/App.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { Tile3DLayer } from "deck.gl";
+import { Tiles3DLoader } from "@loaders.gl/3d-tiles";
+import { MapboxLayer } from "@deck.gl/mapbox/src";
+import "./App.css";
+
+// MapLibreのMapオブジェクト
+let map: maplibregl.Map;
+
+// Appコンポーネント
+export default function App() {
+  const mapContainer = React.useRef<HTMLDivElement | null>(null);
+  // 初期化
+  React.useEffect(() => {
+    if (!map) {
+      if (!mapContainer.current) return;
+      // 地図の作成
+      map = new maplibregl.Map({
+        container: mapContainer.current,
+        style: "https://demotiles.maplibre.org/style.json", // style URL
+        center: [139.753, 35.6844], // 初期緯度経度
+        zoom: 16, // 初期ズーム値
+        minZoom: 4, // 最小ズーム値
+        maxZoom: 20, // 最大ズーム値
+      });
+      map.addControl(new maplibregl.NavigationControl({}), "top-right"); // ズーム・回転コントロールの表示
+      map.addControl(new maplibregl.ScaleControl({}), "bottom-left"); // スケール値の表示
+      map.showTileBoundaries = true; // タイル境界線の表示
+      map.on("load", () => {
+        map.addLayer(
+          new MapboxLayer({
+            id: "tile3dlayer",
+            type: Tile3DLayer,
+            pointSize: 1,
+            data: "https://plateau.geospatial.jp/main/data/3d-tiles/bldg/13100_tokyo/13101_chiyoda-ku/texture/tileset.json",
+            loader: Tiles3DLoader,
+          })
+        );
+      });
+    }
+  });
+
+  return (
+    <div className="App">
+      <div ref={mapContainer} className="map" />
+      <div className="attribution">
+        <a
+          href="https://github.com/Project-PLATEAU/plateau-streaming-tutorial"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          PLATEAU
+        </a>
+        /
+        <a
+          href="https://github.com/maplibre/demotiles"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MapLibre Demo Tiles
+        </a>
+      </div>
+    </div>
+  );
+}

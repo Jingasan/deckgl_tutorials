0df2c5049fb4861b142175e55d2273579cce5f4a
Jinga <info.alpain@gmail.com>
2023-03-19 01:29:36 +0900

    <add> initial commit

---
+++ 00_osm/src/App.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import DeckGL from "@deck.gl/react";
+import { TileLayer, BitmapLayer } from "deck.gl";
+import { InitialViewStateProps } from "@deck.gl/core/lib/deck";
+
+// Appコンポーネント
+export default function App() {
+  // 地図タイルの設定
+  const tileLayer = new TileLayer({
+    data: "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    minZoom: 0,
+    maxZoom: 19,
+    tileSize: 256,
+    renderSubLayers: (props) => {
+      const {
+        bbox: { west, south, east, north },
+      } = props.tile;
+      return new BitmapLayer(props, {
+        data: undefined,
+        image: props.data,
+        bounds: [west, south, east, north],
+      });
+    },
+  });
+  // 初期視点の設定
+  const [viewport, setViewport] = React.useState<InitialViewStateProps>({
+    longitude: 139.753,
+    latitude: 35.6844,
+    zoom: 14,
+    minZoom: 5,
+    maxZoom: 16,
+    pitch: 0,
+    bearing: 0,
+  });
+  // 地図画面
+  return (
+    <div className="App">
+      <DeckGL
+        layers={[tileLayer]}
+        controller={true}
+        initialViewState={viewport}
+      ></DeckGL>
+      <div className="attribution">
+        <a
+          href="https://www.openstreetmap.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open Street Map
+        </a>
+      </div>
+    </div>
+  );
+}
